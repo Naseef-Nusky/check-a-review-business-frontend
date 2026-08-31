4d9f1e77212f577ee39cdae8c45bae93a0233334
Naseef Nusky <naseefnusky09@gmail.com>
2026-08-31 13:15:35 +0530

    google files added

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,11 @@ export default defineConfig({
         target: 'http://localhost:5000',
         changeOrigin: true,
       },
+      '/sitemap.xml': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        rewrite: () => '/business-sitemap.xml',
+      },
       '/uploads': {
         target: 'http://localhost:5000',
         changeOrigin: true,
